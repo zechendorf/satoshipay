bef8f8042a076bf1797e79cfdf7f4c1736eb56ef
Christopher Zechendorf <christopher@zechendorf.com>
2016-07-21 10:25:29 +0200

    cleanup

--- Classes/Hooks/GoodIdHook.php
+++ Classes/Hooks/GoodIdHook.php
@@ -46,42 +46,37 @@ class GoodIdHook
 	
 	private function satoshipayQuery($fieldArray, $action = 'create'){
 		$extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['satoshipay']);
-					
+		
+		// initialize curl
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 		curl_setopt($ch, CURLOPT_USERPWD,$extensionConfiguration['apiKey'].':'.$extensionConfiguration['apiSecret']);
 		
+		// encode the update/create data
+		$data = json_encode(array(
+			'secret' => $fieldArray['secret'],
+			'price' => $fieldArray['price'],
+			'title' => $fieldArray['title'],
+			'url' => ''
+		));
+			
 		if($action=='create'){
 			// set the goods properties
-			$data = json_encode(array(
-				'secret' => $fieldArray['secret'],
-				'price' => $fieldArray['price'],
-				'title' => $fieldArray['title'],
-				'url' => ''
-			));
 			curl_setopt($ch, CURLOPT_URL,'https://api.satoshipay.io/v1/goods');
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');   
 		} else if($action=='update'){
 			// update the goods properties
-			$data = json_encode(array(
-				'secret' => $fieldArray['secret'],
-				'price' => $fieldArray['price'],
-				'title' => $fieldArray['title'],
-				'url' => ''
-			));
 			curl_setopt($ch, CURLOPT_URL,'https://api.satoshipay.io/v1/goods/'.$fieldArray['good_id']);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');  
 		} 
 		
 		curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
-		
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
 			'Content-Type: application/json',                                                                                
 			'Content-Length: ' . strlen($data))                                                                       
 		);
-		
 		
 		// query satoshipay servers
 		$serverResponse = json_decode(curl_exec($ch));
